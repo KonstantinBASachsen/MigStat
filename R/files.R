a0bf4aa5db61e3bd832f67a96b7f7f8dd9a2f829
Konstantin <konstantin.hoffie@ba-sachsen.de>
2022-06-14 12:43:23 +0200

    R/files.R with read_shapes. package builds

--- R/files.R
+++ R/files.R
@@ -1,13 +1,14 @@
-##' Read shape files
+##' Read shape files from path as data.tables
 ##'
 ##' This function reads shapefiles from path. It reads those that
 ##' store information about federal_states, districts and
 ##' municipalities
-##' @title
+##' @title read shape files as data.tables
 ##' @param path path to shape files. Expects directory structure where
 ##'     information for levels is stored in different files
 ##' @return list with data.tables as named elements
-##' @import data.table, sf
+##' @import data.table
+##' sf
 ##' @export read_shapes
 ##' @author Konstantin
 read_shapes <- function(path) {
@@ -17,15 +18,15 @@ read_shapes <- function(path) {
     districts_file <- "VG250_KRS.shp"
 
     munis <- sf::read_sf(paste(path, muni_file, sep = "/"))
-    munis <- data.tablesetDT(munis)
+    munis <- data.table::setDT(munis)
 
     states <- sf::read_sf(paste(path, states_file, sep = "/"))
-    states <- data.tablesetDT(states)
+    states <- data.table::setDT(states)
 
     districts <- sf::read_sf(paste(path, districts_file, sep = "/"))
     districts <- data.table::setDT(districts)
 
     shapes <- list("states" = states, "districts" = districts, "munis" = munis)
-    return(shapes)
     
+    return(shapes)    
     }
