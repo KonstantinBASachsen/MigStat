be0b00b82c3ee4cb176ffe31ae4bc9af275d9a06
Konstantin <konstantin.hoffie@ba-sachsen.de>
2022-06-14 14:20:48 +0200

    test_join_administries now uses read_example

--- inst/tinytest/test_join_administries.R
+++ inst/tinytest/test_join_administries.R
@@ -1,12 +1,10 @@
 library(tinytest)
 
 map_path <- "/home/konstantin/Documents/sexony/inst/extdata/vg250_3112.utm32s.shape.ebenen/vg250_ebenen"
-shapes <- sexony::read_shapes(map_path)
-
 example_path <- "~/network/Rohdaten/Wanderungsdaten FDZ/Dokumente StaLa/WandZuzug_dummy_2010-2013_4480-2021.sav"
 
-df <- foreign::read.spss(example_path, to.data.frame = TRUE)
-dt <- data.table::setDT(df)
+dt <- sexony::read_example(example_path)
+shapes <- sexony::read_shapes(map_path)
 
 sexony::join_administries(dt, shapes$states, shapes$districts, shapes$munis)
 
