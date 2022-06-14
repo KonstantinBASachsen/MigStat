7a8e66c994d9bee2fb9b4e572fe13b1adc60ee67
Konstantin <konstantin.hoffie@ba-sachsen.de>
2022-06-14 12:54:42 +0200

    tinytest/test_read_shapes.R added

---
+++ inst/tinytest/test_read_shapes.R
@@ -0,0 +1,10 @@
+library(tinytest)
+
+map_path <- "/home/konstantin/Documents/sexony/inst/extdata/vg250_3112.utm32s.shape.ebenen/vg250_ebenen"
+shapes <- read_shapes(map_path)
+
+### administrative entities to read
+
+units <- c("federal_states", "districts", "municipalities")
+
+expect_equal(length(shapes), length(units)) 
