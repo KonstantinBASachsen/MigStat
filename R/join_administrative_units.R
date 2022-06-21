ad17b775280b2a4fcf450bcf860fa8f8d1abc282
Konstantin <konstantin.hoffie@ba-sachsen.de>
2022-06-21 10:54:25 +0200

    R/join_administrative_units.R full join optional works

--- R/join_administrative_units.R
+++ R/join_administrative_units.R
@@ -12,62 +12,66 @@
 ##' @import data.table
 ##' @export join_administries
 ##' @author Konstantin
-join_administries <- function(dt, states, districts, municipalities) {
+join_administries <- function(dt, states, districts, municipalities, full) {
 
     dtj <- dt
-    dtj <- join_states(dtj, states)
-    dtj <- join_districts(dtj, districts)
-    dtj <- join_munis(dtj, municipalities)
+    dtj <- join_states(dtj, states, full)
+    dtj <- join_districts(dtj, districts, full)
+    dtj <- join_munis(dtj, municipalities, full)
 
     return(dtj)
 
 }
 
 
-join_states <- function(dt, to_join) {
+join_states <- function(dt, to_join, full) {
 
-    dtj <- join_units(dt, us = "st", to_join = to_join, dest = TRUE)
-    dtj <- join_units(dtj, us = "st", to_join = to_join, dest = FALSE)
-
-    return(dtj)
-}
-
-
-join_districts <- function(dt, to_join) {
-
-    dtj <- join_units(dt, us = "di", to_join = to_join, dest = TRUE)
-    dtj <- join_units(dtj, us = "di", to_join = to_join, dest = FALSE)
+    dtj <- join_units(dt, us = "st", to_join = to_join, dest = TRUE, full)
+    dtj <- join_units(dtj, us = "st", to_join = to_join, dest = FALSE, full)
 
     return(dtj)
 }
 
 
-join_munis <- function(dt, to_join) {
+join_districts <- function(dt, to_join, full) {
 
-    dtj <- join_units(dt, us = "mu", to_join = to_join, dest = TRUE)
-    dtj <- join_units(dtj, us = "mu", to_join = to_join, dest = FALSE)
+    dtj <- join_units(dt, us = "di", to_join = to_join, dest = TRUE, full)
+    dtj <- join_units(dtj, us = "di", to_join = to_join, dest = FALSE, full)
+
+    return(dtj)
+}
+
+
+join_munis <- function(dt, to_join, full) {
+
+    dtj <- join_units(dt, us = "mu", to_join = to_join, dest = TRUE, full)
+    dtj <- join_units(dtj, us = "mu", to_join = to_join, dest = FALSE, full)
     
     return(dtj)
 }
 
-join_units <- function(dt, us, to_join, dest) {
+join_units <- function(dt, us, to_join, dest, full) {
 
     unit <- get_unit(us, dest)
     ags <- get_ags(unit)
 
-    dtj <- do_join(dt, to_join, ags, unit)
+    dtj <- do_join(dt, to_join, ags, unit, full)
     
     return(dtj)
 
 }
 
-do_join <- function(dt, to_join, key, col) {
+do_join <- function(dt, to_join, key, col, full = FALSE) {
     ## performs full join
     i.GEN <- AGS <- NULL
     setkeyv(dt, key)
     setkeyv(to_join, "AGS")
-    unique_keys <- unique(c(dt[, get(key)], to_join[, AGS]))
-    dtu <- dt[unique_keys]
+    if(full == TRUE) {
+        unique_keys <- unique(c(dt[, get(key)], to_join[, AGS]))
+        dtu <- dt[unique_keys]
+    } else {
+        dtu <- dt
+    }
     setkeyv(dtu, key)
     dtu[to_join, (col) := i.GEN]
 
