6913d6ece0173d439c840e13cda5d69e18278cfd
Konstantin <konstantin.hoffie@ba-sachsen.de>
2022-07-19 13:43:20 +0200

    R/simulate_flows.R add_move() added parameter n for number new rows

--- R/simulate_flows.R
+++ R/simulate_flows.R
@@ -1,13 +1,12 @@
-add_move <- function(dt, shps, name_o, name_d, us_o, us_d) {
+add_move <- function(dt, shps, name_o, name_d, us_o, us_d, n) {
 
     GEN <- AGS <- NULL
-    
+
+    n_rows <- nrow(dt)
     unit_o <- get_shpunit(us_o)
     unit_d <- get_shpunit(us_d)
-    
     ags_o <- shps[[unit_o]][GEN == name_o, AGS]
     ags_d <- shps[[unit_d]][GEN == name_d, AGS]
-
     unit_ocol <- get_unitcol(us_o, dest = FALSE)
     unit_dcol <- get_unitcol(us_d, dest = TRUE)
     ags_ocol <- get_agscol(unit_ocol)
@@ -15,11 +14,11 @@ add_move <- function(dt, shps, name_o, name_d, us_o, us_d) {
 
     new_cols <- c(unit_ocol, unit_dcol, ags_ocol, ags_dcol)
     new_values <- c(name_o, name_d, ags_o, ags_d)
-    n_rows <- nrow(dt)
 
     new_row <- as.data.table(t(rep(NA, ncol(dt))))
     colnames(new_row) <- colnames(dt)
     new_row[, (new_cols) := as.list(new_values)]
+    new_row <- new_row[rep(1, n)]
     dtnew <- rbind(dt, new_row)
 
     return(dtnew)
