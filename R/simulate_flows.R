7baa86096f4c8b49821e3439e94f990e3d567810
Konstantin <konstantin.hoffie@ba-sachsen.de>
2022-07-19 14:51:11 +0200

    R/simulate_flows.R add_move stopifnot added, fixed bug

--- R/simulate_flows.R
+++ R/simulate_flows.R
@@ -5,18 +5,19 @@ add_move <- function(dt, shps, name_o, name_d, us_o, us_d, n) {
     n_rows <- nrow(dt)
     unit_o <- get_shpunit(us_o)
     unit_d <- get_shpunit(us_d)
-    ags_o <- shps[[unit_o]][GEN == name_o, AGS]
-    ags_d <- shps[[unit_d]][GEN == name_d, AGS]
+    ags_o <- shps[[unit_o]][GF == 4 & GEN == name_o, AGS]
+    ags_d <- shps[[unit_d]][GF == 4 & GEN == name_d, AGS]
     unit_ocol <- get_unitcol(us_o, dest = FALSE)
     unit_dcol <- get_unitcol(us_d, dest = TRUE)
     ags_ocol <- get_agscol(unit_ocol)
     ags_dcol <- get_agscol(unit_dcol)
 
+    ### better to add columns and then add new row using join?
     stopifnot("Ensure that columns that are joined are already in data.table"
-    = c(unit_ocol, unit_dcol) %in% colnames(dt))
+              = c(unit_ocol, unit_dcol) %in% colnames(dt))
+
     new_cols <- c(unit_ocol, unit_dcol, ags_ocol, ags_dcol)
     new_values <- c(name_o, name_d, ags_o, ags_d)
-
     new_row <- as.data.table(t(rep(NA, ncol(dt))))
     colnames(new_row) <- colnames(dt)
     new_row[, (new_cols) := as.list(new_values)]
