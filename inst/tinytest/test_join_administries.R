4d089706c235fdae8da5286512ca95aad7a645c7
Konstantin <konstantin.hoffie@ba-sachsen.de>
2022-07-19 12:49:28 +0200

    test_join_administries.R get_ags renamed to get_agscol

--- inst/tinytest/test_join_administries.R
+++ inst/tinytest/test_join_administries.R
@@ -15,7 +15,7 @@ expect_equal(nrow(dt), 200) ## if no full join, no rows are added
 dt <- join_administries(dt, shapes$state, shapes$district, shapes$muni, full = TRUE)
 
 
-key <- get_ags(get_unitcol("st", F))
+key <- get_agscol(get_unitcol("st", F))
 expect_equal(length(unique(dt[, ..key][[1]])), 18)
 
 expect_equal(sum(units %in% colnames(dt)), length(units))
