057e477e1dbad1162bb6f1ea7e3545918225764b
Konstantin <konstantin.hoffie@ba-sachsen.de>
2022-08-02 14:00:54 +0200

    R/inkar.R join_inkar()

--- R/inkar.R
+++ R/inkar.R
@@ -1,3 +1,28 @@
+join_inkar <- function(shp, ink) {
+    ### add message that wide == TRUE is expected
+    Kennziffer <- Indikator <- NULL
+    data.table::setkeyv(shp, "AGS")
+    data.table::setkeyv(ink, "Kennziffer")
+    shp_joined <- shp[ink]
+    return(shp_joined)
+
+}
+
+get_inkar_vars <- function(inkar, vars, us, year, wide = TRUE) {
+    Raumbezug <- Zeitbezug <- Indikator <- NULL
+    Kennziffer <- Wert  <- . <- NULL
+        
+    rb <- get_raumbezug(us)
+    ink <- inkar[Raumbezug == rb & Zeitbezug == year & Indikator %in% vars, ]
+    ink <- ink[, .SD[1], by = c("Indikator", "Wert")]
+    ink <- ink[, .(Kennziffer, Indikator, Wert)]
+    if(wide == TRUE) {
+        ink <- data.table::dcast(ink, Kennziffer ~ Indikator, value.var = "Wert")
+    }
+
+    return(ink)
+}
+
 get_raumbezug <- function(us) {
     
     stopifnot(us %in% c("st", "di", "mu"))
@@ -9,27 +34,5 @@ get_raumbezug <- function(us) {
         rb <- "Gemeinden"
     }
     return(rb)
-}
-
-get_inkar_rows <- function(inkar, vars, us, year) {
-    Raumbezug <- Zeitbezug <- Indikator <- NULL
-    Kennziffer <- Wert  <- . <- NULL
-        
-    rb <- get_raumbezug(us)
-    ink <- inkar[Raumbezug == rb & Zeitbezug == year & Indikator %in% vars, ]
-    ink <- ink[, .SD[1], by = c("Indikator", "Wert")]
-    ink <- ink[, .(Kennziffer, Indikator, Wert)]
-
-    return(ink)
-}
-
-join_inkar_rows <- function(shp, ink) {
-    Kennziffer <- Indikator <- NULL
-    data.table::setkeyv(shp, "AGS")
-    data.table::setkeyv(ink, "Kennziffer")
-    ink <- data.table::dcast(ink, Kennziffer ~ Indikator, value.var = "Wert")
-    shp_joined <- shp[ink]
-    return(shp_joined)
-
 }
 
