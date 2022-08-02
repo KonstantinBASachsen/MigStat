3b5b036dc3a032db517fadf819f14774d8d84da2
Konstantin <konstantin.hoffie@ba-sachsen.de>
2022-08-02 13:43:18 +0200

    R/shapefiles.R clean_shp() added

--- R/shapefiles.R
+++ R/shapefiles.R
@@ -1,3 +1,13 @@
+clean_shp <- function(shps, us, keep =  c("AGS", "GEN", "EWZ", "geometry")) {
+
+    AGS <- NULL
+    
+    shp_clean <- get_shp_region(shps, us, drop_gf = TRUE)
+    shp_clean[, "AGS" := as.numeric(AGS)] ## should print message or warning 
+    shp_clean <- keep_cols(shp_clean, keep)
+    return(shp_clean)
+}
+
 get_shp_region <- function(shps, us, drop_gf) {
     GF <- NULL
     shp <- shps[[get_shp_unit(us)]]
