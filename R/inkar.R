300d0d76fb6760b2d4f658c64bf4336c6f468b22
Konstantin <konstantin.hoffie@ba-sachsen.de>
2022-08-02 13:08:17 +0200

    R/inkar.R added get_raumbezug(), get_inkarrows(), join_inkarrows()

---
+++ R/inkar.R
@@ -0,0 +1,32 @@
+get_raumbezug <- function(us) {
+    
+    stopifnot(us %in% c("st", "di", "mu"))
+    if (us == "st") {
+        rb <- "Bundesländer"
+    } else if (us == "di") {
+        rb <- "Kreise"
+    } else {
+        rb <- "Gemeinden"
+    }
+    return(rb)
+}
+
+get_inkarrows <- function(inkar, vars, us, year) {
+    Raumbezug <- Zeitbezug <- Indikator <- Kennziffer <- Wert <- NULL
+    rb <- get_raumbezug(us)
+    ink <- inkar[Raumbezug == rb & Zeitbezug == zb & Indikator %in% cols]
+    ink <- ink[, .SD[1], by = c("Indikator", "Wert")]
+    ink <- ink[, .(Kennziffer, Indikator, Wert)]
+
+    return(ink)
+}
+
+join_inkarrows <- function(shp, ink) {
+    Kennziffer <- Indikator <- NULL
+    setkeyv(shp, "AGS")
+    setkeyv(ink, "Kennziffer")
+    ink <- dcast(ink, Kennziffer ~ Indikator, value.var = "Wert")
+    shp_joined <- shp[ink]
+    return(shp_joined)
+
+}
