ce223c587d73a18d383fb04abc7362700688af3c
Konstantin <konstantin.hoffie@ba-sachsen.de>
2022-08-03 14:51:02 +0200

    documentation for read_examples(), changed shp_path

--- R/files.R
+++ R/files.R
@@ -1,7 +1,20 @@
-read_examples <- function(mig_path = "~/network/Rohdaten/Wanderungsdaten FDZ/Dokumente StaLa/WandZuzug_dummy_2010-2013_4480-2021.sav", shp_path = "/home/konstantin/Documents/sexony/inst/extdata/vg250_3112.utm32s.shape.ebenen/vg250_ebenen" ) {
+##' Reads the example migration statistics data set and one
+##' shapefile. Mainly used to quickly load data for unit tests.
+##'
+##' 
+##' @title Read example migration statistics and corresponding
+##'     shapefile.
+##' @param mig_path Path to migration statistics
+##' @param shp_path path to shapefile
+##' @return named list with example migration statistics as first
+##'     element and shapefile as second.
+##' @author Konstantin
+read_examples <- function(mig_path =
+"~/network/Rohdaten/Wanderungsdaten FDZ/Dokumente StaLa/WandZuzug_dummy_2010-2013_4480-2021.sav",
+shp_path = "/home/konstantin/Diss/inst/extdata/vg250-ew_3112.utm32s.shape.ebenen/vg250-ew_ebenen") {
 
     dt <- read_migex(mig_path)
-    shps <- read_shapes(shp_path) #
+    shps <- read_shapes(shp_path) 
     data <- list("mig" = dt, "shps" = shps)
 
     return(data)
