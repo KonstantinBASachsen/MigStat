fcc121ef02e0bbd4e8e2946ed93824784cde12fa
Konstantin <konstantin.hoffie@ba-sachsen.de>
2022-08-03 12:49:26 +0200

    documentation for join_inkar_vars() added

--- R/inkar.R
+++ R/inkar.R
@@ -1,3 +1,32 @@
+##' The inkar data set holds very many interesting variables on
+##' different regional levels and for different years. This function
+##' joins given variables to a data table from a shapefile.
+##'
+##' The function assumes that the id column is called "AGS".
+##'
+##' The package is designed in a way that assumes that shp comes from
+##' the function clean_shp; although that is not required.
+##' 
+##' @title Join variables from inkar data set to table.
+##' @param shp The data.table with at least one column called "AGS"
+##'     which holds the identifier for regions.
+##' @param inkar inkar data.table
+##' @param vars character vector with variable names from inkar data
+##'     that are to be joined.
+##' @param us unit_simple: "st", "di" or "mu".
+##' @param year The yearwhich is to be joined. Assumed to be a
+##'     character .
+##' @return data.table with joined variables.
+##' @import data.table
+##' @export join_inkar_vars
+##' @author Konstantin
+join_inkar_vars <- function(shp, inkar, vars, us, year) {
+    ink <- get_inkar_vars(inkar, vars, us, year)
+    shp_ink <- join_inkar(shp, ink)
+    return(shp_ink)
+}
+
+
 join_inkar <- function(shp, ink) {
     ### add message that wide == TRUE is expected
     Kennziffer <- Indikator <- NULL
