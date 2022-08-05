0430780f628317f73a1fedc09aafed5194e2f301
Konstantin <konstantin.hoffie@ba-sachsen.de>
2022-08-05 12:31:00 +0200

    tests updated, all tests pass

--- inst/tinytest/test_arrow_plot.R
+++ inst/tinytest/test_arrow_plot.R
@@ -3,27 +3,31 @@ ex_dat <- read_examples()
 shps <- ex_dat$shps
 dtj <- join_administries(ex_dat$mig, shps$state, shps$district, shps$muni, full = FALSE) ### join "official" names
 
+us <- "st"
 name <- "Berlin"
-dtf <- get_arrow_data(dt = dtj, shapes = shps, name = name, o_us = "di",
-               d_us = "st")
+
+dtf <- get_flows(ex_dat$mig, shps, us)
+shp <- clean_shp(ex_dat$shps, us = us)
+dtarrow <- get_arrow_data(dtf, shp, name)
+
 
 ### should be exactly one origin, todo: rename dest to origin
-expect_equal(dtf[origin == TRUE, .N], 1)
+expect_equal(dtarrow[o_region == TRUE, .N], 1)
 
 ### if origin is also destination, there are two places with the
 ### "name", this is the case for Berlin
-expect_equal(sum(grepl(name, dtf[, place])), 2) 
+## expect_equal(sum(grepl(name, dtf[, place])), 2) does not apply anymore
 
 
 
 name <- "Mecklenburg-Vorpommern"
-dtf <- get_arrow_data(dt = dtj, shapes = shps, name = name, o_us = "st",
-               d_us = "st")
+dtarrow <- get_arrow_data(dtf, shp, name)
 
 ### should be exactly one origin, todo: rename dest to origin
-expect_equal(dtf[origin == TRUE, .N], 1)
+expect_equal(dtarrow[o_region == TRUE, .N], 1)
 
 ### if origin is also destination, there are two places with the
 ### "name", this is not the case for Mecklenburg-Vorpommern
-expect_equal(sum(grepl(name, dtf[, place])), 1) 
+
+expect_equal(sum(grepl(get_ags(shp, name), dtarrow[, place])), 1) 
 
