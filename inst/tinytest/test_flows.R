37419d7b2f431a2057871f70f1c427edeba5c1be
Konstantin <konstantin.hoffie@ba-sachsen.de>
2022-08-17 15:40:50 +0200

    all tests ok

--- inst/tinytest/test_flows.R
+++ inst/tinytest/test_flows.R
@@ -4,10 +4,10 @@ shps <- ex_dat$shps
 dt <- join_administries(ex_dat$mig, shps$state, shps$district, shps$muni, full = TRUE)
 
 losses <- get_losses(dt, "di")
-expect_equal(sum(losses[is.na(district_o) & !is.na(EF03U4), flow]), 34)
+expect_equal(sum(losses[is.na(region) & !is.na(ags), flow]), 34)
 
 wins <- get_wins(dt, "mu")
-expect_equal(wins[muni_d == "München", flow], 8)
+expect_equal(wins[region == "München", flow], 8)
 
 ### add test if sum flows after get_flows equals rows of data.table
 ### add test if nrow(get_flows()) == n_regions^2
