e8809e24984d6b80cd94459ed834d108a6d30935
Konstantin <konstantin.hoffie@ba-sachsen.de>
2022-10-05 13:05:27 +0200

    distances from od-flows.R to distances.R

---
+++ R/distances.R
@@ -0,0 +1,36 @@
+join_distances <- function(region_pairs, distances) {
+    i.distance <- origin <- destination <- NULL
+    combs <- copy(region_pairs)
+    dist <- copy(distances)
+    combs <- combs[, "od" := paste(origin, destination, sep = "_")]
+    dist <- dist[, "od" := paste(origin, destination, sep = "_")]
+    setkeyv(combs, "od")
+    setkeyv(dist, "od")
+    combs[dist, "distance" := i.distance]
+    return(combs)
+    
+}
+
+get_distances <- function(shp) {
+
+    EWZ <- geometry <- centers <- AGS <- i.GEN <- . <- destination <-
+        origin <- od <- i.distance <- NULL
+    ### computes pair wise distances between all units of type
+### "us". Maybe I only need it for pairs with non zero flows?
+    shp_dist <- copy(shp)
+    shp_dist[, "centers" := st_centroid(geometry)]
+    distances <- round(st_distance(shp_dist[, centers] / 1000), 0) 
+    keys <- shp_dist[, AGS]
+    colnames(distances) <- keys
+    rownames(distances) <- keys
+
+    distances <- setDT(data.frame(distances, check.names = FALSE))
+    distances$destinations <- colnames(distances)
+    dist_pairs <- melt(distances, id.vars = "destinations")
+    colnames(dist_pairs) <- c("destination", "origin", "distance")
+    dist_pairs[shp_dist, "o_name" := i.GEN, on = .(origin = AGS)]
+    dist_pairs[shp_dist, "d_name" := i.GEN, on = .(destination = AGS)]
+
+    return(dist_pairs)
+
+}
--- R/od-flows.R
+++ R/od-flows.R
@@ -42,7 +42,10 @@ get_flows <- function(dt, shp, us, dist = FALSE, full = FALSE, pops = FALSE, na_
     ### I think I should not compute and join the distances
     ### here. Maybe this belongs to some other function
     
-    ### Don't know why this function needs state_o and so on cols
+### Don't know why this function needs state_o and so on cols
+
+    ### I don't like join_populations() in here and I don't know
+    ### anymore why I needed this. Probably for gravity sampling
     flow <- NULL
     flows <- get_flows_only(dt, us)
     if (full == TRUE) {
@@ -69,42 +72,6 @@ get_flows <- function(dt, shp, us, dist = FALSE, full = FALSE, pops = FALSE, na_
 }
 
 
-join_distances <- function(region_pairs, distances) {
-    i.distance <- origin <- destination <- NULL
-    combs <- copy(region_pairs)
-    dist <- copy(distances)
-    combs <- combs[, "od" := paste(origin, destination, sep = "_")]
-    dist <- dist[, "od" := paste(origin, destination, sep = "_")]
-    setkeyv(combs, "od")
-    setkeyv(dist, "od")
-    combs[dist, "distance" := i.distance]
-    return(combs)
-    
-}
-
-get_distances <- function(shp) {
-
-    EWZ <- geometry <- centers <- AGS <- i.GEN <- . <- destination <-
-        origin <- od <- i.distance <- NULL
-    ### computes pair wise distances between all units of type
-### "us". Maybe I only need it for pairs with non zero flows?
-    shp_dist <- copy(shp)
-    shp_dist[, "centers" := st_centroid(geometry)]
-    distances <- round(st_distance(shp_dist[, centers] / 1000), 0) 
-    keys <- shp_dist[, AGS]
-    colnames(distances) <- keys
-    rownames(distances) <- keys
-
-    distances <- setDT(data.frame(distances, check.names = FALSE))
-    distances$destinations <- colnames(distances)
-    dist_pairs <- melt(distances, id.vars = "destinations")
-    colnames(dist_pairs) <- c("destination", "origin", "distance")
-    dist_pairs[shp_dist, "o_name" := i.GEN, on = .(origin = AGS)]
-    dist_pairs[shp_dist, "d_name" := i.GEN, on = .(destination = AGS)]
-
-    return(dist_pairs)
-
-}
 
 join_missing_regions <- function(flows, shp) {
 
