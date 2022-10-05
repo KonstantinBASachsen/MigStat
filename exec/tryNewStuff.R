1e9f86bb1a443ef8db1a4a5d062cb4550c90b169
Konstantin <konstantin.hoffie@ba-sachsen.de>
2022-10-05 14:45:36 +0200

    get_flows_only() grouping seems to work

--- exec/tryNewStuff.R
+++ exec/tryNewStuff.R
@@ -1,6 +1,8 @@
 ex_dat <- read_examples()
 mig <- ex_dat$mig
 mig$gender <- sample(c("m", "f"), nrow(mig), replace = TRUE)
+mig$age_gr <- sample(c("0-6", "7-16", "16-99"), nrow(mig), replace = TRUE)
+    
 us <- "st"
 shp <- clean_shp(ex_dat$shps, us)
 flows <- get_flows(dt = mig, shp = shp, us = us, by = "gender", full = FALSE, dist = TRUE)
@@ -17,21 +19,13 @@ get_flows_only <- function(dt, us, by = NULL, simplify = TRUE) {
     if (simplify == TRUE) {
         dtf <- dtf[, .SD, .SDcols = c(ags_o, ags_d, by, "flow")]
         dtf <- dtf[, .SD[1], by = c(ags_o, ags_d, by)]
-        if (is.null(by)) {
-            dtf <- dtf[, .(get(ags_o), get(ags_d), flow)] ### making sure cols are in right order
-            colnames(dtf) <- c("origin", "destination", "flow")
-        } else {
-            dtf <- dtf[, .(get(ags_o), get(ags_d), get(by), flow)] ### making sure cols are in right order
-            colnames(dtf) <- c("origin", "destination", by, "flow")
+        dtf[, c("origin", "destination") := .(get(ags_o), get(ags_d))]
+        dtf[, c(ags_o, ags_d) := NULL]
+        data.table::setcolorder(dtf, c("origin", "destination", by, "flow"))
         }
-    }
-#### should do this in a separate step    
-##    dtf <- dtf[, c(ags_o, ags_d) := lapply(.SD, as.numeric), .SDcols = c(ags_o, ags_d)]
     return(dtf)
 }
 
-flows <- get_flows_only(mig, us = us, by = "gender", simplify = TRUE)
+flows <- get_flows_only(mig, us = us, by = c("gender", "age_gr"), simplify = TRUE)
 flows2 <- get_flows_only(mig, us = us, simplify = TRUE)
 
-is.null(NULL)
-flows[origin == "08" & destination == "09", ]
