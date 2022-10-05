bc0dbe392415da4b2007802d066ac802bd081737
Konstantin <konstantin.hoffie@ba-sachsen.de>
2022-10-05 16:32:21 +0200

    discovered bug in get_flows(), get_flows_only() works!

--- exec/tryNewStuff.R
+++ exec/tryNewStuff.R
@@ -6,3 +6,25 @@ mig$age_gr <- sample(c("0-6", "7-16", "16-99"), nrow(mig), replace = TRUE)
 us <- "st"
 shp <- clean_shp(ex_dat$shps, us)
 flows <- get_flows(dt = mig, shp = shp, us = us, by = "gender", full = FALSE, dist = TRUE)
+flows <- get_flows(dt = mig, shp = shp, us = us, full = TRUE, dist = FALSE)
+
+
+flows[, sum(flow), by = destination]
+flows[, sum(flow), by = origin]
+
+mig[EF02U2 == "02", ][, .N]
+
+get_agscol("state_o")
+
+print(flows[order(origin)], 200)
+
+
+mig[, .N, by = EF03U2][order(EF03U2)]
+
+flows[origin == "04"]
+mig[EF03U2 == "04"]
+unique(mig[, EF03U2])
+
+flows <- get_flows_only(dt = mig,  us = "st")
+flows[, sum(flow), by = destination]
+flows[, sum(flow), by = origin][order(origin)] == mig[, .N, by = EF03U2][order(EF03U2)]
