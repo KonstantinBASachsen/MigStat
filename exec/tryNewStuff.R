b630556e568e144e51e23c66f96d3ae5031c1321
Konstantin <konstantin.hoffie@ba-sachsen.de>
2022-10-06 13:46:48 +0200

    code deleted

--- exec/tryNewStuff.R
+++ exec/tryNewStuff.R
@@ -9,61 +9,6 @@ flows <- get_flows(dt = mig, shp = shp, us = us, by = c("gender", "age_gr"), ful
 flows <- get_flows(dt = mig, shp = shp, us = us, full = TRUE, dist = FALSE, na_to_0=TRUE)
 
 
-
-get_wins2 <- function(flows, grouped = TRUE, by = NULL) {
-    wins <- copy(flows)
-    if (grouped == TRUE) {
-        if (is.null(by)) {
-            nogroup <- c("origin", "destination", "flow", "distance")
-            by <- setdiff(colnames(flows), nogroup)
-        }
-        message(sprintf("wins grouped by '%s' are returned", paste(by, collapse = ", ")))
-
-        wins <- wins[, "wins" := sum(flow), by = c("destination", by)]
-        wins <- wins[, .SD[1], by = c("destination", by)]
-        wins <- wins[order(destination)]
-        wins <- wins[, c("flow", "distance", "origin") := NULL]
-    } else {
-        wins <- wins[, "wins" := sum(flow), by = c("destination")]
-        wins <- wins[, .SD[1], by = c("destination")]
-        wins <- wins[order(destination)]
-        wins <- wins[, "region" := destination]
-        wins <- wins[, .(region, wins)]
-    }
-    return(wins)
-}
-
-wins <- get_wins2(flows, FALSE)
-wins[, sum(wins)]
-
-
-get_losses2 <- function(flows, grouped = TRUE, by = NULL) {
-    losses <- copy(flows)
-    if (grouped == TRUE) {
-        if (is.null(by)) {
-            nogroup <- c("origin", "destination", "flow", "distance")
-            by <- setdiff(colnames(flows), nogroup)
-        }
-        message(sprintf("losses grouped by '%s' are returned", paste(by, collapse = ", ")))
-
-        losses <- losses[, "losses" := sum(flow), by = c("origin", by)]
-        losses <- losses[, .SD[1], by = c("origin", by)]
-        losses <- losses[order(origin)]
-        losses <- losses[, c("flow", "distance", "destination") := NULL]
-    } else {
-        losses <- losses[, "losses" := sum(flow), by = c("origin")]
-        losses <- losses[, .SD[1], by = c("origin")]
-        losses <- losses[order(origin)]
-        losses <- losses[, "region" := origin]
-        losses <- losses[, .(region, losses)]
-    }
-    return(losses)
-}
-
-
-losses <- get_losses2(flows, TRUE)
-
-
 get_grouped <- function(flows, region, grouped = TRUE, by = NULL) {
     stopifnot(region %in% c("origin", "destination"))
     if(region == "origin") {
@@ -95,22 +40,7 @@ get_grouped <- function(flows, region, grouped = TRUE, by = NULL) {
     return(losses)
 }
 
-test <- get_grouped(flows, "destination", TRUE)
-test <- get_grouped(flows, "origin", FALSE)
-test[order(region)]
-test[region == "09", sum(losses)]
-
-mig[EF03U2 == "09", ]
-
-test <- get_grouped(flows, "destination")
-
-test[region == "09", sum(losses)]
-
-mig[EF02U2 == "09", ]
-var <- "wins"
-test[,  paste(var) := type]
-
-paste(var)
-
-test[, .(get("gender"), get("region"), get(var))]
-
+wins <- get_grouped(flows = flows, region = "destination", grouped = TRUE, by = "gender")
+var <- "gender"
+type <- "wins"
+colnames(flows) %in% c("region", type)
