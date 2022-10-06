649f21a45024cda4022e11e118a1b06888e4aa8c
Konstantin <konstantin.hoffie@ba-sachsen.de>
2022-10-06 10:25:59 +0200

    tests now use new get_wins(), get_losses()

--- inst/tinytest/test_flows.R
+++ inst/tinytest/test_flows.R
@@ -1,13 +1,34 @@
 ex_dat <- read_examples()
+mig <- ex_dat$mig
 shps <- ex_dat$shps
 
-dt <- join_administries(ex_dat$mig, shps$state, shps$district, shps$muni, full = TRUE)
 
-losses <- get_losses(dt, "di")
-expect_equal(sum(losses[is.na(region) & !is.na(ags), flow]), 34)
+#################### get_wins() and get_losses() ###############################
+################################################################################
 
-wins <- get_wins(dt, "mu")
-expect_equal(wins[region == "München", flow], 8)
+us <- "mu"
+shp <- clean_shp(shps, us)
+## dt <- join_administries(ex_dat$mig, shps$state, shps$district, shps$muni, full = TRUE)
+flows <- get_flows(mig, shp, us)
+losses <- get_losses(flows)
+expect_equal(losses[region == "09162000", losses], 6)
+
+wins <- get_wins(flows)
+expect_equal(wins[region == "11000000", wins], 6)
 
 ### add test if sum flows after get_flows equals rows of data.table
+expect_equal(losses[, sum(losses)], nrow(mig))
+expect_equal(wins[, sum(wins)], nrow(mig))
 ### add test if nrow(get_flows()) == n_regions^2!
+
+
+mig[EF02U5 == "11000000"]
+
+get_agscol("muni_o")
+mig
+colnames(mig)
+
+
+########################### get_net() ##########################################
+################################################################################
+
