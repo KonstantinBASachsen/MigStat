ad459cee7c10aa11d1cb09fa436c5b424749536d
Konstantin <konstantin.hoffie@ba-sachsen.de>
2022-10-06 15:06:06 +0200

    include_missing_obs() fills missing group combinations

--- exec/tryNewStuff.R
+++ exec/tryNewStuff.R
@@ -10,5 +10,31 @@ flows <- get_flows(dt = mig, shp = shp, us = us, full = TRUE, dist = FALSE, na_t
 
 
 
-get_wins(flows, TRUE)
+wins <- get_wins(flows, TRUE)[order(region)]
+
+keys <- setDT(expand.grid(unique(wins[, region]), unique(wins[, gender])))
+keys <- keys[order(Var1)]
+colnames(keys) <- c("region", "gender")
+
+setkeyv(wins, c("region", "gender" ))
+wins[keys]
+
+
+values <- list("region" = shp[, AGS], "gender" = c("m", "f"), "age_gr" = c("0-6", "7-16", "16-99"))
+values
+
+
+
+names(values)
+
+include_missing_obs <- function(dt, values, missing_col) {
+    ms <- missing_col
+    stopifnot("missing_col not in data.table" = ms %in% colnames(dt))
+    keys <- do.call(data.table::CJ, values)
+    setkeyv(dt, names(values))
+    dtfull <- dt[keys, ]
+    dtfull <- dtfull[order(mget(names(values)))]
+    dtfull <- dtfull[is.na(get(ms)), paste(ms)  := 0]
+    return(dtfull)
+}
 
