674690187dd16bf84d3ebb6932fcddcb5c42a56c
Konstantin <konstantin.hoffie@ba-sachsen.de>
2022-10-06 09:17:32 +0200

    new get_net(), get_wins() and get_losses() moved to flows.R

--- R/flows.R
+++ R/flows.R
@@ -1,126 +1,137 @@
 ##' computes the difference of in-migration and out-migration for all
-##' regions of one of the following types: states, districts,
-##' municipalities.
+##' regions in the od-flow data. Wins and losses are returned as well.
 ##'
 ##' The function makes sure that regions where no people in-migrated
 ##' or out-migrated are handled approbpriately. That is, missing
 ##' in-migration or out-migration is treated as 0.
 ##' @title net migration for regions
-##' @param mig data.table of Migration Statistics
-##' @param us One of: "st", "di" or "mu", short hand for "states",
-##'     "districts", "municipalities".
-##' @return data.table with region, ags and net flow.
+##' @param flows data.table holding od-flows. Returned by get_flows().
+##' @return data.table with ags, net flow and wins and losses.
 ##' @import data.table
 ##' @export get_net
 ##' @author Konstantin
-get_net <- function(mig, us) {
-    ags <- flow <- region <- i.flow <- i.region <- NULL
-    w <- get_wins(mig, us)
-    l <- get_losses(mig, us)
-    keys <- unique(c(w[, ags], l[, ags]))
-    data.table::setkeyv(w, "ags")
-    data.table::setkeyv(l, "ags")
+get_net <- function(flows) {
+    flow <- region <- i.flow <- i.region <- NULL
+    w <- get_wins(flows)
+    l <- get_losses(flows)
+    keys <- unique(c(w[, region], l[, region]))
+    data.table::setkeyv(w, "region")
+    data.table::setkeyv(l, "region")
     w <- w[keys, ]
-    w[is.na(flow), "flow" := 0]
+    w[is.na(wins), "wins" := 0]
     l <- l[keys, ]
-    l[is.na(flow), "flow" := 0]
-    data.table::setkeyv(w, "ags")
-    data.table::setkeyv(l, "ags")
-    w[l, "flow" := flow - i.flow]
-    ### this next lines ensure that if there are regions in losses
-    ### that are not in wins, the name of the region is transfered to
-    ### wins
+    l[is.na(losses), "losses" := 0]
+    data.table::setkeyv(w, "region")
+    data.table::setkeyv(l, "region")
+    w[l, "losses" := i.losses]
+    w[, "net" := wins - losses]
+    setcolorder(w, c("region", "net", "wins", "losses"))
+    ### the next lines ensure that if there are regions in losses that
+    ### are not in wins, the name of the region is transferred to wins
     row <- w[is.na(region)][l, "region" := i.region]
     idx <- which(is.na(w[, region]))
     w[idx] <- row
     return(w)
 }
 
-
-##' Get wins (in-migration) for regions
-##'
-##' This function returns the number of in-migrations for every region
-##' of the specified type in the data.table.
-##' @title Get wins per region
-##' @param dt data.table
-##' @param us "unit simple". One of the following strings: "st"
-##'     (federal_states), "di" (districts) or "mu" (municipalities).
-##' @return data.table with columns: unit, ags, flow
-##' @export get_wins
-##' @author Konstantin
-get_wins <- function(dt, us) {
-
-    wins <- get_flow(dt, us, T)
-
+get_wins <- function(flows) {
+    wins <- flows[, sum(flow), by = destination]
+    wins <- wins[, .(region = destination, wins = V1)]
     return(wins)
 }
 
-
-##' Get losses (out-migration) for regions
-##'
-##' This function returns the number of out-migrations for every region
-##' of the specified type in the data.table.
-##' @title Get losses per region
-##' @param dt data.table
-##' @param us "unit simple". One of the following strings: "st"
-##'     (federal_states), "di" (districts) or "mu" (municipalities).
-##' @return data.table with columns: unit, ags, flow
-##' @export get_losses
-##' @author Konstantin
-get_losses <- function(dt, us) {
-
-    losses <- get_flow(dt, us, F)
-
-    return(losses)    
+get_losses <- function(flows) {
+    losses <- flows[, sum(flow), by = origin]
+    losses <- losses[, .(region = origin, losses = V1)]
+    return(losses)
 }
 
 
-get_flow <- function(dt, us, dest) {
+## ##' Get wins (in-migration) for regions
+## ##'
+## ##' This function returns the number of in-migrations for every region
+## ##' of the specified type in the data.table.
+## ##' @title Get wins per region
+## ##' @param dt data.table
+## ##' @param us "unit simple". One of the following strings: "st"
+## ##'     (federal_states), "di" (districts) or "mu" (municipalities).
+## ##' @return data.table with columns: unit, ags, flow
+## ##' @export get_wins
+## ##' @author Konstantin
+## get_wins <- function(dt, us) {
 
-### us unit short: abbreviation for administrative unit, either "st"
-### for state, "di" for district or "mu" for municipality. If dest =
-### T, then destinations are considered, that is: wins. If dest = F,
-### then origins are considered, that is, losses
+##     wins <- get_flow(dt, us, T)
+
+##     return(wins)
+## }
+
+
+## ##' Get losses (out-migration) for regions
+## ##'
+## ##' This function returns the number of out-migrations for every region
+## ##' of the specified type in the data.table.
+## ##' @title Get losses per region
+## ##' @param dt data.table
+## ##' @param us "unit simple". One of the following strings: "st"
+## ##'     (federal_states), "di" (districts) or "mu" (municipalities).
+## ##' @return data.table with columns: unit, ags, flow
+## ##' @export get_losses
+## ##' @author Konstantin
+## get_losses <- function(dt, us) {
+
+##     losses <- get_flow(dt, us, F)
+
+##     return(losses)    
+## }
+
+
+
+## get_flow <- function(dt, us, dest) {
+
+## ### us unit short: abbreviation for administrative unit, either "st"
+## ### for state, "di" for district or "mu" for municipality. If dest =
+## ### T, then destinations are considered, that is: wins. If dest = F,
+## ### then origins are considered, that is, losses
     
-    unit <- get_unitcol(us, dest)
-    ags <- get_agscol(unit)
-    dtf <- dt
-    dtf[, "flow" := .N, by = ags]
-    flows <- dtf[, .SD[1], by = ags]
-    flows <- flows[, .SD, .SDcols = c(unit, ags, "flow")]
-    dt[, "flow" := NULL]
-    colnames(flows)[1] <- "region"
-    colnames(flows)[2] <- "ags"
-    return(flows)
-}
+##     unit <- get_unitcol(us, dest)
+##     ags <- get_agscol(unit)
+##     dtf <- dt
+##     dtf[, "flow" := .N, by = ags]
+##     flows <- dtf[, .SD[1], by = ags]
+##     flows <- flows[, .SD, .SDcols = c(unit, ags, "flow")]
+##     dt[, "flow" := NULL]
+##     colnames(flows)[1] <- "region"
+##     colnames(flows)[2] <- "ags"
+##     return(flows)
+## }
 
 
-##' In-migration, out-migration or net-migration can be viwed as an
-##' attribute of regions. join_to_shp() takes a data.table and joins
-##' it to the data.table holding the shape information.
-##'
-##' @title join wins, losses or net migration to data.table of
-##'     shapefile
-##' @param shp data.table holding the shape information. Expected to
-##'     have column "AGS" that is used to join.
-##' @param dt data.table holding the flows (either wins, losses, or
-##'     net). Might be output from get_wins(), get_losses() or
-##'     get_net().
-##' @param col Name of new column in returned shp data.table.
-##' @param key_dt Name of column in dt that holds the AGS.
-##' @return shp data.table with joined column
-##' @import
-##' data.table
-##' @export
-##' join_to_shp
-##' @author Konstantin
-join_to_shp <- function(shp, dt, col, key_dt = "ags") {
-    i.flow <- NULL
-    shpj <- data.table::copy(shp)
-    data.table::setkeyv(shpj, "AGS")
-    data.table::setkeyv(dt, key_dt)
-    shpj[dt, (col) := i.flow]
-    return(shpj)
-}
+## ##' In-migration, out-migration or net-migration can be viwed as an
+## ##' attribute of regions. join_to_shp() takes a data.table and joins
+## ##' it to the data.table holding the shape information.
+## ##'
+## ##' @title join wins, losses or net migration to data.table of
+## ##'     shapefile
+## ##' @param shp data.table holding the shape information. Expected to
+## ##'     have column "AGS" that is used to join.
+## ##' @param dt data.table holding the flows (either wins, losses, or
+## ##'     net). Might be output from get_wins(), get_losses() or
+## ##'     get_net().
+## ##' @param col Name of new column in returned shp data.table.
+## ##' @param key_dt Name of column in dt that holds the AGS.
+## ##' @return shp data.table with joined column
+## ##' @import
+## ##' data.table
+## ##' @export
+## ##' join_to_shp
+## ##' @author Konstantin
+## join_to_shp <- function(shp, dt, col, key_dt = "ags") {
+##     i.flow <- NULL
+##     shpj <- data.table::copy(shp)
+##     data.table::setkeyv(shpj, "AGS")
+##     data.table::setkeyv(dt, key_dt)
+##     shpj[dt, (col) := i.flow]
+##     return(shpj)
+## }
 
 
--- exec/tryNewStuff.R
+++ exec/tryNewStuff.R
@@ -9,39 +9,4 @@ flows <- get_flows(dt = mig, shp = shp, us = us, by = "gender", full = FALSE, di
 flows <- get_flows(dt = mig, shp = shp, us = us, full = TRUE, dist = FALSE, na_to_0=TRUE)
 
 
-get_wins <- function(flows) {
-    wins <- flows[, sum(flow), by = destination]
-    wins <- wins[, .(region = destination, wins = V1)]
-    return(wins)
-}
-
-get_losses <- function(flows) {
-    losses <- flows[, sum(flow), by = origin]
-    losses <- losses[, .(region = origin, losses = V1)]
-    return(losses)
-}
-
-get_net <- function(flows) {
-    flow <- region <- i.flow <- i.region <- NULL
-    w <- get_wins(flows)
-    l <- get_losses(flows)
-    keys <- unique(c(w[, region], l[, region]))
-    data.table::setkeyv(w, "region")
-    data.table::setkeyv(l, "region")
-    w <- w[keys, ]
-    w[is.na(wins), "wins" := 0]
-    l <- l[keys, ]
-    l[is.na(losses), "losses" := 0]
-    data.table::setkeyv(w, "region")
-    data.table::setkeyv(l, "region")
-    w[l, "losses" := i.losses]
-    w[, "net" := wins - losses]
-    setcolorder(w, c("region", "net", "wins", "losses"))
-    ### the next lines ensure that if there are regions in losses that
-    ### are not in wins, the name of the region is transferred to wins
-    row <- w[is.na(region)][l, "region" := i.region]
-    idx <- which(is.na(w[, region]))
-    w[idx] <- row
-    return(w)
-}
 
