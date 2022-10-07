ca04c08ed061c7c6f5d01a144a7154068b810c2a
Konstantin <konstantin.hoffie@ba-sachsen.de>
2022-10-07 09:40:44 +0200

    deleted code

--- exec/tryNewStuff.R
+++ exec/tryNewStuff.R
@@ -19,48 +19,10 @@ colnames(keys) <- c("region", "gender")
 setkeyv(wins, c("region", "gender" ))
 wins[keys]
 
+all_regions <- c(shp[, AGS], "00")
+values <- list("region" = all_regions, "gender" = c("m", "f"), "age_gr" = c("0-6", "7-16", "16-99"))
+values2 <- list("origin" = all_regions, "destination" = all_regions, "gender" = c("m", "f"))
 
-values <- list("region" = c(shp[, AGS], "00"), "gender" = c("m", "f"), "age_gr" = c("0-6", "7-16", "16-99"))
-values
-
-
-
-names(values)
-
-include_missing_obs <- function(dt, values, missing_col) {
-    ms <- missing_col
-    stopifnot("missing_col not in data.table" = ms %in% colnames(dt))
-    keys <- do.call(data.table::CJ, values)
-    setkeyv(dt, names(values))
-    dtfull <- dt[keys, ]
-    dtfull <- dtfull[order(mget(names(values)))]
-    dtfull <- dtfull[is.na(get(ms)), paste(ms)  := 0]
-    return(dtfull)
-}
-
-get_net <- function(flows, values, grouped, by = NULL) {
-
-    ### probably a good idea to supply "values" only optional and
-    ### otherwise read them from the columns. Although then there
-    ### might be some combinations missing that are not in the data
-
-    ### maybe grouped is not really necessary because values already
-    ### gives the grouping information
-    
-    wins <- get_wins(flows, grouped, by)
-    wins <- include_missing_obs(wins, values, "wins")
-    losses <- get_losses(flows, grouped, by)
-    losses <- include_missing_obs(losses, values, "losses")
-    setkeyv(wins, names(values))
-    setkeyv(losses, names(values))
-    wins[losses, "losses" := i.losses]
-    wins[, "net" := wins - losses]
-    return(wins)
-}
-
-net <- get_net(flows, values[1], FALSE)
-
-net[, sum(wins)]
-net[, sum(losses)]
-net[, sum(net)]
-?do.call
+flows <- get_flows(mig, shp, "st", by = "gender", values = values2)
+flows[order(origin, destination)]
+flows[origin == "01", ]
