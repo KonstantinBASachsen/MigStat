b317119e3be1be540fa3291e477333d1eca1b18d
Konstantin <konstantin.hoffie@ba-sachsen.de>
2022-10-07 11:44:11 +0200

    get_regions()

--- exec/tryNewStuff.R
+++ exec/tryNewStuff.R
@@ -2,9 +2,33 @@ ex_dat <- read_examples()
 mig <- ex_dat$mig
 mig$gender <- sample(c("m", "f"), nrow(mig), replace = TRUE)
 mig$age_gr <- sample(c("0-6", "7-16", "16-99"), nrow(mig), replace = TRUE)
+
+get_regions <- function(dt, shps, us, type) {
+    stopifnot(type %in% c("data", "all"))
+    ags_o <- get_agscol(get_unitcol(us, F))
+    ags_d <- get_agscol(get_unitcol(us, T))
+    shp <- clean_shp(shps, us)
+    if (type == "data") {
+        all_regions <- unique(c(dt[, get(ags_o)], dt[, get(ags_d)]))
+    }
+    if (type == "all") {
+        all_regions <- unique(c(shp[, AGS], dt[, get(ags_o)], dt[, get(ags_d)]))
+    }
+    return(all_regions)
+}
+
+
+
     
 us <- "st"
 shp <- clean_shp(ex_dat$shps, us)
-flows <- get_flows(dt = mig, shp = shp, us = us, by = c("gender", "age_gr"), dist = TRUE)
-flows <- get_flows(dt = mig, shp = shp, us = us, full = TRUE, dist = FALSE, na_to_0=TRUE)
+
+regions <- get_regions(mig, shps, us, "all")
+values <- list("origin" = regions, "destination" = regions,
+               "gender" = c("m", "f"), "age_gr" = c("0-6", "7-16", "16-99"))
+flows <- get_flows(dt = mig, shp = shp, us = us, by = c("gender", "age_gr"), values = values)
+
+net <- get_net(flows, values)
+net[, sum(losses)]
+net[, sum(wins)]
 
