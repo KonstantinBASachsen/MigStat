f74555b55cb36a217d5173abfc80d335914bbadc
Konstantin <konstantin.hoffie@ba-sachsen.de>
2022-10-25 16:17:31 +0200

    group_regions() takes shp as argument

--- R/add_variables.R
+++ R/add_variables.R
@@ -1,9 +1,17 @@
-group_regions <- function(mig, ags_names) {
+group_regions <- function(mig, shp) {
     ags_sachsen <- "14" 
     ags_bl <- as.character(1:16)
     ags_bl[1:9] <- paste0("0", ags_bl[1:9])
     ags_ost <- c("12", "13", "15", "16") ### ohne Berlin, ohne Sachsen
     ags_west <- setdiff(ags_bl, c(ags_sachsen, ags_ost)) #### mit Berlin
+
+#### Hier greife ich aus dem shapefile die AGS für unsere
+#### interessanten Gemeinden heraus. Dies sollte bestenfalls im
+#### korrekten Jahr geschehen, da sich die AGS durchaus ändern
+#### können. Dies implementiere ich später.
+    regions <- c("Hoyerswerda", "Riesa", "Oschatz", "Delitzsch", "Altenburg")
+    ags_names <- shp[GEN %in% regions, .(AGS, GEN)]
+    
     mig[, "group_o" := as.character(NA)]
     mig[EF03U2 %in% ags_west, group_o := "west"]
     mig[EF03U2 %in% ags_ost, group_o := "ost"]
@@ -14,7 +22,7 @@ group_regions <- function(mig, ags_names) {
     mig[EF03U5 %in% ags_names[3, AGS], group_o := ags_names[3, GEN]]
     mig[EF03U5 %in% ags_names[4, AGS], group_o := ags_names[4, GEN]]
     mig[EF03U5 %in% ags_names[5, AGS], group_o := ags_names[5, GEN]]
-
+    message(mig[, .N, by = group_o])
     mig[, "group_d" := as.character(NA)]
     mig[EF02U2 %in% ags_west, group_d := "west"]
     mig[EF02U2 %in% ags_ost, group_d := "ost"]
@@ -25,5 +33,6 @@ group_regions <- function(mig, ags_names) {
     mig[EF02U5 %in% ags_names[3, AGS], group_d := ags_names[3, GEN]]
     mig[EF02U5 %in% ags_names[4, AGS], group_d := ags_names[4, GEN]]
     mig[EF02U5 %in% ags_names[5, AGS], group_d := ags_names[5, GEN]]
+    message(mig[, .N, by = group_d])
     return(NULL)
 }
