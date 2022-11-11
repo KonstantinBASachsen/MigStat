cea0c3c23575a8134715a36f0cefd8eb84709f2a
Konstantin <konstantin.hoffie@ba-sachsen.de>
2022-11-11 11:20:21 +0100

    exec/make_shapes.R creates shape tables, assumes processed shapes

---
+++ exec/make_shapes.R
@@ -0,0 +1,12 @@
+library("MigStat")
+
+## This script assumes that the shapefiles are downloaded and
+## preprocessed. These steps are done by download_shapes.R in
+## Diss/exec. I plan to write some function that does this and which
+## will be part of MigStat.
+
+shps_path <- "~/extdata/shapes31_test/no_ewz"
+clean_path <- "~/extdata/shapes31simple2/"
+
+years <- 2000:2018
+regions <- clean_shps(shps_path, clean_path, years, "complete")
