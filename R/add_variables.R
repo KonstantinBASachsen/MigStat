bde895ecaf5ad67f86e190376dcdc396a984fe79
Konstantin <konstantin.hoffie@ba-sachsen.de>
2022-11-15 13:43:53 +0100

    built notes fixed, newly installed

--- R/add_variables.R
+++ R/add_variables.R
@@ -1,6 +1,5 @@
 group_regions <- function(mig, ags_gen) {
-    GEN <- . <- AGS <- EF03U2 <- group_o <- EF03U5 <- NULL
-    EF02U2 <- group_d <- EF02U5 <- NULL
+    EF03U2 <- EF02U2 <- i.GEN <- group_o <- group_d <- NULL
     ags_sachsen <- 14
     ags_bl <- 1:16
     ags_ost <- c(12, 13, 15, 16) ### ohne Berlin, ohne Sachsen
@@ -10,7 +9,6 @@ group_regions <- function(mig, ags_gen) {
     mig[EF03U2 %in% ags_west, group_o := "west"]
     mig[EF03U2 %in% ags_ost, group_o := "ost"]
     mig[EF03U2 %in% ags_sachsen, group_o := "sachsen"]
-    
     setkeyv(mig, "EF03U5")
     setkeyv(ags_gen, "AGS")
     mig[ags_gen, "group_o" := i.GEN]
@@ -20,7 +18,6 @@ group_regions <- function(mig, ags_gen) {
     mig[EF02U2 %in% ags_west, group_d := "west"]
     mig[EF02U2 %in% ags_ost, group_d := "ost"]
     mig[EF02U2 %in% ags_sachsen, group_d := "sachsen"]
-
     setkeyv(mig, "EF02U5")
     setkeyv(ags_gen, "AGS")
     mig[ags_gen, "group_d" := i.GEN]
