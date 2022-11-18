0876f38e8ab048042702e76cceaeb22e31a6d4d8
Konstantin <konstantin.hoffie@ba-sachsen.de>
2022-11-18 11:33:05 +0100

    empty_regions() to add empty region for plotting

--- R/add_variables.R
+++ R/add_variables.R
@@ -53,5 +53,26 @@ group_regions <- function(mig, ags_gen, add_empty = FALSE) {
     setkeyv(ags_gen, "AGS")
     mig[ags_gen, "group_d" := i.GEN]
     message(mig[, .N, by = group_d])
+    if (add_empty == TRUE) {
+        empty_region(mig)
+        message("group_o, group_d now factors with NA level so in plots comparison groups ost, west, Sachsen are in the bottom")
+    }
+    return(NULL)
+}
+
+empty_region <- function(mig) {
+    ## This I should integrate into group_regions or maybe I shouldn't
+    ## because of the empty factor level
+    ## Hier bringe ich die Regionen in die richtige Reihenfolge und
+    ## bereite sie für das Erstellen von Grafiken vor.
+    group_o <- group_d <- NULL
+    regions <- c("Altenburg", "Delitzsch", "Hoyerswerda", "Oschatz",
+                 "Riesa", "sachsen", "ost", "west")
+    ## Ich füge "" als factor level hinzu, damit an der richtigen Stelle
+    ## im Plot ein freies Feld ist. Ist nicht super, aber ich weiß nicht,
+    ## wie ich ggplot sagen kann, dass ein facet frei bleiben soll.
+    lvls <- c(regions[1:5], "", regions[6:8])
+    mig[, "group_o" := factor(group_o, levels = c(lvls, NA))]
+    mig[, "group_d" := factor(group_d, levels = c(lvls, NA))]
     return(NULL)
 }
