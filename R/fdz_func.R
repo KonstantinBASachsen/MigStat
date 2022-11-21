fa98c417959e55380be0c6af93e14dbb6db25fb1
Konstantin <konstantin.hoffie@ba-sachsen.de>
2022-11-21 10:37:36 +0100

    doc for all functions in fdz_func.R

--- R/fdz_func.R
+++ R/fdz_func.R
@@ -13,6 +13,7 @@ read_mig <- function(path, year) {
     dt <- dt[, .SD, .SDcols = cols]
     return(dt)
 }
+
 ##' Creates five point summary for faster box plots
 ##'
 ##' 
@@ -28,7 +29,9 @@ get_box_data <- function(mig, col, by) {
     ### boxplots verwende. Die Zusammenfassung wird für "col"
     ### berechnet. Jeweils für alle Gruppen welche durch "by". Kann
     ### auch ggplot automatisch machen, dauert dann aber deutlich
-    ### länger.
+### länger.
+
+    ## replace fivenum with boxplot.stats?
     dt_box <- mig[, stats::fivenum(get(col)), by = by]
     stats <- c("ymin", "y25", "ymed", "y75", "ymax")
     dt_box[, "what" := stats, by = by]
@@ -42,25 +45,54 @@ get_box_data <- function(mig, col, by) {
     return(dt_box)
 }
 
+##' Makes a boxplot from table returned by get_box_data(). Expects
+##' "year_short" as x-axis and "group" as faceting variable
+##'
+##' 
+##' @title Boxplot with facets
+##' @param dt data.table returned by get_box_data()
+##' @param title Plot title
+##' @return plot object
+##' @author Konstantin
+##' @import ggplot2
 custom_boxplot <- function(dt, title) {
     ## Plotted die 5 Punkte Zusammenfassung, welche mit get_box_data()
     ## erstellt wurde.
     group <- NULL
-    box_dist <- ggplot(dt, aes_string("year_short")) +
-        geom_boxplot(aes_string(x = "year_short", min = "ymin", lower = "y25",
-                     middle = "ymed", upper = "y75", max = "ymax",
-                     group = "year"), stat = "identity") +
-    facet_wrap(vars(group), drop = FALSE) +
-    MigStat::theme_brrrp(maj.y = TRUE) +
-    theme(
-        axis.text.x = element_text(angle = 45)) +
-    ggtitle(title) +
-    xlab("Jahr") +
-    ylab("Distanz in km")
+    box_dist <- ggplot2::ggplot(dt, ggplot2::aes_string("year_short")) +
+        ggplot2::geom_boxplot(
+                     ggplot2::aes_string(x = "year_short",
+                                         min = "ymin", lower = "y25",
+                                         middle = "ymed", upper = "y75", max = "ymax",
+                                         group = "year"), stat = "identity") +
+    ggplot2::facet_wrap(ggplot2::vars(group), drop = FALSE) +
+    theme_brrrp(maj.y = TRUE) +
+    ggplot2::theme(
+        axis.text.x = ggplot2::element_text(angle = 45)) +
+    ggplot2::ggtitle(title) +
+    ggplot2::xlab("Jahr") +
+    ggplot2::ylab("Distanz in km")
     return(box_dist)
 }
 
-
+##' Retunes a common data.table of wins and losses and returnes the
+##' net migration (wins - losses). Only designed to handle wins and
+##' losses with respect to federal states.
+##'
+##' @title net migration of wins and losses of regions with respect to
+##'     federal states.
+##' @param wins data.table wih "wins" (number of moves into a region)
+##'     per regions
+##' @param losses data.table wih "losses" (number of moves out of a
+##'     region) per regions
+##' @param states data.table with columns AGS and GEN of federal
+##'     states
+##' @param ags_gen data.table with columns AGS and GEN of our
+##'     interesting regions. This is neccessary to recognize that
+##'     different ags might belong to the same region.
+##' @return data.table with names of regions and their wins, losses
+##'     and net migration with respect to federal states.
+##' @author Konstantin
 get_net2 <- function(wins, losses, states, ags_gen) {
     ## Erstellt gemeinsame data.table aus wins und losses unsberechnet
     ## die Netto Migration
@@ -87,38 +119,55 @@ get_net2 <- function(wins, losses, states, ags_gen) {
     return(net)
 }
 
-
+##' Plots net migration of regions wrt. federal states. Best used with
+##' input from get_net2.
+##'
+##' @title plotting net migration wrt. to federal states
+##' @param net data.table with regions and net migration wrt to
+##'     federal states. Preferably output from get_net2().
+##' @param lab_years Which years are plotted? lab_years is inserted in the title
+##' @return gpglot object
+##' @author Konstantin
 plot_age_st <- function(net, lab_years) {
     ## erstellt plot Gewinne/Verluste nach Bundesländern
     name_r <- NULL
-  title <- sprintf("Gewinne/ Verluste der Regionen nach Bundesland \n der Jahre %s", lab_years)
-  plot <- ggplot(net) +
-    geom_col(aes_string("name_bl", "net", fill = "age_gr"), 
-             position = "dodge") +
-    MigStat::theme_brrrp(maj.x = TRUE) +
-    coord_flip() +
-    labs(fill = "Alter gruppiert") +
-    facet_wrap(vars(name_r)) +
-    theme_brrrp(leg.pos = c(0.85, 0.2), maj.x= FALSE, 
-                maj.y = TRUE)  +
-    ylab("Gewinn/Verlust") +
-    xlab("") +
-    ggtitle(title)
-  return(plot)
+    title <- sprintf("Gewinne/ Verluste der Regionen nach Bundesland \n der Jahre %s", lab_years)
+    plot <- ggplot2::ggplot(net) +
+        ggplot2::geom_col(ggplot2::aes_string("name_bl", "net", fill = "age_gr"), 
+                          position = "dodge") +
+        theme_brrrp(maj.x = TRUE) +
+        ggplot2::coord_flip() +
+        ggplot2::labs(fill = "Alter gruppiert") +
+        ggplot2::facet_wrap(ggplot2::vars(name_r)) +
+        theme_brrrp(leg.pos = c(0.85, 0.2), maj.x= FALSE, 
+                    maj.y = TRUE)  +
+        ggplot2::ylab("Gewinn/Verlust") +
+        ggplot2::xlab("") +
+        ggplot2::ggtitle(title)
+    return(plot)
 }
 
+##' plots number of moves to or from a region (depending on the input)
+##' for different age groups over the course of years
+##'
+##' @title plots age distribution of moves
+##' @param dt data.table with columns year, flow, age_gr and grp. Grp
+##'     is used as facet variable
+##' @param title Title of plot
+##' @return ggplot object
+##' @author Konstantin
 plot_age_dist <- function(dt, title) {
     ## Erstellt plots mit Altersverteilung
     grp <- NULL
-  plot_age <- ggplot(stats::na.omit(dt, "grp")) +
-    geom_line(aes_string("year", "flow", colour = "age_gr")) +
-    facet_wrap(vars(grp), scales = "free", drop = FALSE) +
-    MigStat::theme_brrrp(leg.pos = c(0.85, 0.5), maj.y = TRUE) +
-    ylab("Anzahl") +
-    xlab("Jahr") +
-    labs("colour" = "Alter gruppiert") +
-    ggtitle(title) +
-    theme(axis.text.x = element_text(angle = 45))
+    plot_age <- ggplot2::ggplot(stats::na.omit(dt, "grp")) +
+        ggplot2::geom_line(ggplot2::aes_string("year", "flow", colour = "age_gr")) +
+        ggplot2::facet_wrap(ggplot2::vars(grp), scales = "free", drop = FALSE) +
+        theme_brrrp(leg.pos = c(0.85, 0.5), maj.y = TRUE) +
+        ggplot2::ylab("Anzahl") +
+        ggplot2::xlab("Jahr") +
+        ggplot2::labs("colour" = "Alter gruppiert") +
+        ggplot2::ggtitle(title) +
+        ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45))
     
   return(plot_age)
 }
