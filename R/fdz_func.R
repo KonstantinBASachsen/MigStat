433f6c82c865ab4087ad3b0cb4bc4baf294f6bdb
Konstantin <konstantin.hoffie@ba-sachsen.de>
2022-11-21 11:21:52 +0100

    make_paths() added

--- R/fdz_func.R
+++ R/fdz_func.R
@@ -6,6 +6,24 @@
 ## functions designed to ease a specific task. They are not general or
 ## robust enough to be used in other settings.
 
+
+##' Sets paths for current working environment
+##'
+##' @title sets paths for working at the FDZ or at work
+##' @param p_work paths for work
+##' @param p_fdz paths for FDZ
+##' @param fdz logical, working at the fdz or not?
+##' @return list with paths
+##' @author Konstantin
+make_paths <- function(p_work, p_fdz, fdz) {
+    stopifnot(is.logical(fdz))
+    if (fdz == TRUE) {
+        paths <- p_fdz
+    } else {
+        paths <- p_work
+    }
+}
+
 read_mig <- function(path, year) {
   ### liest die Wanderungsdaten ein
     dt <- data.table::fread(path, encoding = "UTF-8")
