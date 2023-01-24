a330857eda111fa25c5fdf7486b2fb5a2b741a26
Konstantin <konstantin.hoffie@ba-sachsen.de>
2023-01-24 09:36:31 +0100

    get_net2() to flows.R

--- R/flows.R
+++ R/flows.R
@@ -1,3 +1,53 @@
+##' Retunes a common data.table of wins and losses and returnes the
+##' net migration (wins - losses). Only designed to handle wins and
+##' losses with respect to federal states.
+##'
+##' @title net migration of wins and losses of regions with respect to
+##'     federal states.
+##' @param wins data.table wih "wins" (number of moves into a region)
+##'     per regions
+##' @param losses data.table wih "losses" (number of moves out of a
+##'     region) per regions
+##' @param states data.table with columns AGS and GEN of federal
+##'     states
+##' @param ags_gen data.table with columns AGS and GEN of our
+##'     interesting regions. This is neccessary to recognize that
+##'     different ags might belong to the same region.
+##' @return data.table with names of regions and their wins, losses
+##'     and net migration with respect to federal states.
+##' @author Konstantin
+get_net2 <- function(wins, losses, states, ags_gen) {
+    ## Erstellt gemeinsame data.table aus wins und losses unsberechnet
+    ## die Netto Migration
+    i.GEN <- . <- AGS <- age_gr <- state <- name_r <- NULL
+    keys_w <- colnames(wins)[! colnames(wins) %in% c("flow", "wins")]
+    keys_l <- colnames(losses)[!colnames(losses) %in% c("flow", "losses")]
+    net <- merge(wins, losses, by.x = keys_w, by.y = keys_l, all = TRUE)
+    colnames(net)[colnames(net) == "flow.x"] <- "wins"
+    colnames(net)[colnames(net) == "flow.y"] <- "losses"
+    colnames(net)[colnames(net) == "destination"] <- "region"
+    colnames(net)[colnames(net) == "EF03U2"] <- "state"
+    net[ags_gen, "name_r" := i.GEN, on = .(region = AGS)]
+    net[states, "name_bl" := i.GEN, on = .(state = AGS)]
+    ## the following line makes sure that regions with different ags
+    ## are treated as one. In this case the number of groups/ rows
+    ## might be reduced. Might because if year is taken as grouping
+    ## variable than different ags for the same region correspond to
+    ## different years and because year is a group it does not reduce
+    ## the overall number of groups.
+    net <- net[, "wins" := sum(wins), by = .(age_gr, state, name_r, year)]
+    net <- net[, "losses" := sum(losses), by = .(age_gr, state, name_r, year)]
+    net <- net[, .SD[1], by = .(age_gr, state, name_r, year)]
+    net[is.na(wins), "wins" := 0]
+    net[is.na(losses), "losses" := 0]
+    net[, "net" := wins - losses]
+    ## net <- net[, "net" := sum(net), by = .(age_gr, state, name_r, year)]
+    ## net <- net[, .SD[1], by = .(age_gr, state, name_r, year)]
+### alternatively????
+##    net <- net[, .(net = sum(net))]
+    return(net)
+}
+
 ##' computes the difference of in-migration and out-migration for all
 ##' regions in the od-flow data. Wins and losses are returned as well.
 ##'
