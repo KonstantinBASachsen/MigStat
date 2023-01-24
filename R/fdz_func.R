6ccb77bcd20c405be8ffcbc0b2eab85b5b164a32
Konstantin <konstantin.hoffie@ba-sachsen.de>
2023-01-24 09:41:36 +0100

    file deleted

--- R/fdz_func.R
+++
@@ -1,9 +0,0 @@
-######################################################################
-####################### Purpose of script ############################
-######################################################################
-
-## Here I collect functions that I use in the fdz. These are helper
-## functions designed to ease a specific task. They are not general or
-## robust enough to be used in other settings.
-
-
