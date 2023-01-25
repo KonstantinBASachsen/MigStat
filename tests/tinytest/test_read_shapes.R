fd73b32bf2d95be5651e77165c0f9095222a5d72
Konstantin <konstantin.hoffie@ba-sachsen.de>
2023-01-25 11:39:27 +0100

    fine

--- tests/tinytest/test_read_shapes.R
+++ tests/tinytest/test_read_shapes.R
@@ -1,9 +1,9 @@
-
+library("MigStat")
+library("tinytest")
 shp_path <- "~/extdata/shapes31_test/no_ewz"
 shapes <- read_shapes(shp_path)
 
 ### administrative entities to read
-
 units <- c("federal_states", "districts", "municipalities")
-
 expect_equal(length(shapes), length(units)) 
+### script does not increase test coverage
