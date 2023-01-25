91288a47783c6f194981804871889fa3e2f6520e
Konstantin <konstantin.hoffie@ba-sachsen.de>
2023-01-25 11:40:51 +0100

    fine

--- tests/tinytest/test_shapefiles.R
+++ tests/tinytest/test_shapefiles.R
@@ -1,5 +1,8 @@
+library("MigStat")
+library("tinytest")
 clean_path <- "~/extdata/shapes31simple/"
-regions <- read_clean_shps(clean_path)
+regions <- MigStat:::read_clean_shps(clean_path)
 
 expect_equal(nrow(regions$districts), 478)
 expect_equal(nrow(regions$munis), 16874)
+#### script does not increase test coverage
