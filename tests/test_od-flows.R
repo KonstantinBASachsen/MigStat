74d1a3454958dacda07f176ea148ee35e76a05f9
Konstantin <konstantin.hoffie@ba-sachsen.de>
2023-01-25 10:33:51 +0100

    tests/test_od-flows works

--- tests/test_od-flows.R
+++ tests/test_od-flows.R
@@ -1,14 +1,16 @@
-ex_dat <- read_examples()
+library("MigStat")
+library("tinytest")
+ex_dat <- MigStat:::read_examples()
 mig <- ex_dat$mig
 ### Here I create additional variables to check if the summary of
 ### flows between regions also works when data is grouped
 mig[, "gender" := c(rep("m", 100), rep("f", 100))]
-mig[, "age_gr" := fifelse(EF25 < 35, "0-35", ">35")]
+mig[, "age_gr" := data.table::fifelse(EF25 < 35, "0-35", ">35")]
 
-shp <- clean_shp(ex_dat$shps, "st", keep = c("AGS"))
+shp <- MigStat:::clean_shp(ex_dat$shps, "st", keep = c("AGS"))
 all_regions <- c(shp[, AGS], "00")
 values <- list("origin" = all_regions, "destination" = all_regions)
-flows <- get_flows(dt = mig,  us = "st", values = values)
+flows <- MigStat::get_flows(dt = mig,  us = "st", values = values)
 
 ### all rows should be included in flows
 expect_equal(flows[, sum(flow)], 200)
@@ -31,14 +33,14 @@ expect_equal(length(unique(nchar(flows$destination))), 1)
 n_regions <- length(unique(shp[, AGS]))
 expect_true(nrow(flows) >= n_regions^2, info = sprintf("expected %s, got %s", n_regions^2, nrow(flows)))
 
-shp <- clean_shp(ex_dat$shps, "di", keep = "AGS")
+shp <- MigStat:::clean_shp(ex_dat$shps, "di", keep = "AGS")
 all_regions <- c(shp[, AGS], "00000") ### does not work
 all_regions2 <- unique(c(mig[, EF03U4], mig[, EF02U4]))
 all_regions3 <- unique(c(all_regions, all_regions2))
 ## setdiff(all_regions2, all_regions)
 
 values <- list("origin" = all_regions3, "destination" = all_regions3)
-flows <- get_flows(dt = mig, us = "di", values = values)
+flows <- MigStat::get_flows(dt = mig, us = "di", values = values)
 ### all rows should be included in flows
 expect_equal(flows[, sum(flow)], 200)
 
@@ -52,7 +54,7 @@ expect_true(nrow(flows) >= n_regions^2, info = sprintf("expected %s, got %s", n_
 ######################################################################
 
 ## mig[EF02U2 == "09" & EF03U2 == "08"]
-flows <- get_flows(dt = mig, by = "gender", us = "st")
+flows <- MigStat::get_flows(dt = mig, by = "gender", us = "st")
 expect_equal(flows[origin == "08" & destination == "09", flow], c(6, 5))
 
 flows <- get_flows(dt = mig, us = "st", by = c("gender", "age_gr"))
