df7fda62b73595976a2ce377f1eadd5a74a67719
Konstantin <konstantin.hoffie@ba-sachsen.de>
2023-01-25 10:12:30 +0100

    deleted tests to modify one by one

--- tests/test_add_variables.R
+++ tests/test_add_variables.R
@@ -1,7 +1,7 @@
 ex_dat <- MigStat:::read_examples() ## still reads AGS cols as character...
 mig <- ex_dat$mig
 shps <- ex_dat$shps
-shps <- read_clean_shps("/home/konstantin/extdata/shapes31simple2", type = "complete")
+shps <- MigStat:::read_clean_shps("/home/konstantin/extdata/shapes31simple2", type = "complete")
 
 
 ### I want to check if variables are added correctly. Particularly, if
@@ -17,9 +17,9 @@ mig[, "EF02U2" := as.integer(EF03U2)]
 mig[, "EF03U2" := as.integer(EF03U2)]
 mig[, "EF03U5" := as.integer(EF03U5)]
 mig[, "EF02U5" := as.integer(EF02U5)]
-mig <- add_vars(mig, ags_gen = ags_gen, add_empty = FALSE)
-expect_equal(uniqueN(mig[, group_o]), 5)
-expect_equal(uniqueN(mig[, group_d]), 5)
-expect_equal(mig[, .N, by = age_gr][, N], c(75, 90, 20, 15))
-expect_equal(mig[, .N, by = year_gr][, N], 200)
+mig <- MigStat:::add_vars(mig, ags_gen = ags_gen, add_empty = FALSE)
+tinytest::expect_equal(data.table::uniqueN(mig[, group_o]), 5)
+tinytest::expect_equal(data.table::uniqueN(mig[, group_d]), 5)
+tinytest::expect_equal(mig[, .N, by = age_gr][, N], c(75, 90, 20, 15))
+tinytest::expect_equal(mig[, .N, by = year_gr][, N], 200)
 
--- tests/test_read_shapes.R
+++
@@ -1,9 +0,0 @@
-
-shp_path <- "~/extdata/shapes31_test/no_ewz"
-shapes <- read_shapes(shp_path)
-
-### administrative entities to read
-
-units <- c("federal_states", "districts", "municipalities")
-
-expect_equal(length(shapes), length(units)) 
