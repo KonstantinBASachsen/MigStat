5ed6389aa32afb41815fd7396bd0dea72ded082f
Konstantin <konstantin.hoffie@ba-sachsen.de>
2023-01-25 10:43:03 +0100

    works

---
+++ tests/test_shapefiles.R
@@ -0,0 +1,8 @@
+library("MigStat")
+library("tinytest")
+clean_path <- "~/extdata/shapes31simple/"
+regions <- MigStat:::read_clean_shps(clean_path)
+
+expect_equal(nrow(regions$districts), 478)
+expect_equal(nrow(regions$munis), 16874)
+#### script does not increase test coverage
--- tests/tinytest/test_shapefiles.R
+++
@@ -1,5 +0,0 @@
-clean_path <- "~/extdata/shapes31simple/"
-regions <- read_clean_shps(clean_path)
-
-expect_equal(nrow(regions$districts), 478)
-expect_equal(nrow(regions$munis), 16874)
