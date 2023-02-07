e503fe5b3d44fa32044c67e77fd5998ac254cbee
Konstantin <konstantin.hoffie@ba-sachsen.de>
2023-02-07 13:26:08 +0100

    new make_paths()

--- R/make_paths.R
+++ R/make_paths.R
@@ -1,22 +1,25 @@
 ##' Sets paths for current working environment
 ##'
 ##' @title sets paths for working at the FDZ or at work
-##' @param p_work paths for work
-##' @param p_fdz paths for FDZ
-##' @param fdz logical, working at the fdz or not?
+##' @param which Character that specifies which default paths are to
+##'     be created.
+##' @param paths Optional, if new paths are to be used. Must contain
+##'     paths 'mig' for migration statistics, 'shps' for shapefiles,
+##'     'dist' for distances and 'fig' for output of figures.
 ##' @return list with paths
+##' @export make_paths
 ##' @author Konstantin
 make_paths <- function(which = c("work", "kamenz", "dresden"), paths = NULL) {
     which <- match.arg(which)
     p_dresden <- list(
         ### not needed anymore
-        fig = "E:\\FDZ_II\\Guckel\\Übergaben\\2022-11-02\\Graphiken/",
+        fig = "E:\\FDZ_II\\Guckel\\Uebergaben\\2022-11-02\\Graphiken/",
         mig = "E:\\FDZ_II\\Guckel\\Daten\\Zuzug",
         shps = "E:\\FDZ_II\\Guckel\\Daten\\shapeFiles\\Shapefiles_281022\\4480_Shapefiles_281022",
         data = "E:\\FDZ_II\\Guckel\\Daten"
     )
     p_kamenz <- list(
-        fig = "H:\\Übergaben\\2022-11-25\\output",
+        fig = "H:\\Uebergaben\\2022-11-25\\output",
         mig = "H:\\Daten",
         shps = "H:\\Daten\\shapes",
         dist = "H:\\Daten\\Distanzen"
@@ -31,9 +34,12 @@ make_paths <- function(which = c("work", "kamenz", "dresden"), paths = NULL) {
                       "work" = p_work)
     if (is.null(paths)) {
         paths <- path_list[[which]]
+        if (which == "dresden" | which == "kamenz") {
+            warning("paths not correct because non ASCII characters")
+        }
     } else {
         needed_paths <- c("mig", "shps", "dist", "fig")
-        stopifnot(needed_paths %in% names(paths))
+        stopifnot("paths must contain 'mig', 'shps', 'dist' and 'fig'" = needed_paths %in% names(paths))
     }
     return(paths)
 }
