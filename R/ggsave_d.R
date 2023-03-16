de850ea5f41d921d4ed6ae32430e14d02f26f067
Konstantin <konstantin.hoffie@ba-sachsen.de>
2023-03-16 15:58:19 +0100

    now save_plot() works but ggsave_d() not!

--- R/ggsave_d.R
+++ R/ggsave_d.R
@@ -53,7 +53,7 @@ ggsave_d <- function(plt, plot_name, path, save_data = FALSE,
         if(is.null(data) == TRUE & save_data == TRUE) { ## should go in return_data_base
             stop("'plt' seems to be base-r, please provide data for saving or set save_data = FALSE.")
         }
-        base_save(plot = plt, plot_name = plot_name, path = ps$plot_path)
+        base_save(plt = plt, plot_name = plot_name, path = ps$plot_path)
         ## I should write return_data_base that checks if plt returns
         ## a data.table and if so, returns this table and if not, returns data
         dt <- data.table::copy(data)
@@ -70,11 +70,21 @@ save_plot <- function(plt, plot_name, path, save_data = FALSE,
         stop("Detected '.' in plot_name. Please specify without file ending.")
     }
     ps <- make_plot_dirs(path)
-    base_save(plt = plt, plot_name = plot_name, path = ps$plot_path)
-    dt <- data.table::copy(data)
-    dt <- drop_geometry(dt)
-    saving_plot_data(dt = dt, save_data = save_data, excel = excel,
-                     plot_name = plot_name, paths = ps)
+    ## if (inherits(plt, "gg")) {
+    ##     ggplot2::ggsave(filename = paste0(plot_name, ".pdf"), plot = plt, path = ps$plot_path, ...)
+    ##     dt <- return_data_gg(plot = plt, data = data)
+    ## } else {
+    ##     if(is.null(data) == TRUE & save_data == TRUE) { ## should go in return_data_base
+    ##         stop("'plt' seems to be base-r, please provide data for saving or set save_data = FALSE.")
+#        }
+        base_save(plt = plt, plot_name = plot_name, path = ps$plot_path)
+        ## I should write return_data_base that checks if plt returns
+        ## a data.table and if so, returns this table and if not, returns data
+ #       dt <- data.table::copy(data)
+    ## }
+    ## dt <- drop_geometry(dt)
+    ## saving_plot_data(dt = dt, save_data = save_data, excel = excel,
+    ##                  plot_name = plot_name, paths = ps)
 }
 
 base_save <- function(plt, plot_name, path) {
