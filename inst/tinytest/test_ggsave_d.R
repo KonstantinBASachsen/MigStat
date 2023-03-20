36868a64f17805c0488fff2866838251a318c8b7
Konstantin <konstantin.hoffie@ba-sachsen.de>
2023-03-20 12:53:26 +0100

    tests for save_plot()

---
+++ inst/tinytest/test_ggsave_d.R
@@ -0,0 +1,35 @@
+temp_path <- "~/MigStat/inst/temp"
+
+#### Does plot file exist?
+fn <- "test_base"
+save_plot(plot(cars), fn, temp_path, base = TRUE, save_data = FALSE)
+file <- file.path(temp_path, "plots", paste0(fn, ".pdf"))
+expect_true(file.exists(file))
+
+## errors should pop up
+f_call <- expr(save_plot(plot(cars), paste0(fn, ".pdf"), temp_path,
+                         base = TRUE, save_data = FALSE))
+expect_error(eval(f_call), pattern = "file ending")
+
+f_call <- expr(save_plot(plot(cars), fn, temp_path,
+                         base = TRUE, save_data = TRUE))
+expect_error(eval(f_call), pattern = "saving")
+
+## saving data as either xlsx or csv
+dt <- data.table(cars)
+f_call <- expr(save_plot(plot(cars), fn, temp_path,
+                         base = TRUE, save_data = TRUE,
+                         data = dt))
+file <- file.path(temp_path, "plot_data", paste0(fn, ".xlsx"))
+eval(f_call)
+expect_true(file.exists(file))
+
+f_call <- expr(save_plot(plot(cars), fn, temp_path,
+                         base = TRUE, save_data = TRUE,
+                         data = dt, excel = FALSE))
+file <- file.path(temp_path, "plot_data", paste0(fn, ".csv"))
+eval(f_call)
+expect_true(file.exists(file))
+
+### delete created folder
+unlink(temp_path, recursive = TRUE)
