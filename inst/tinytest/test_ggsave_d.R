c9271fc8c9d13930eacd68cf645c329e31afa804
Konstantin <konstantin.hoffie@ba-sachsen.de>
2023-03-20 12:59:34 +0100

    test expr() -> expression()

--- inst/tinytest/test_ggsave_d.R
+++ inst/tinytest/test_ggsave_d.R
@@ -7,24 +7,24 @@ file <- file.path(temp_path, "plots", paste0(fn, ".pdf"))
 expect_true(file.exists(file))
 
 ## errors should pop up
-f_call <- expr(save_plot(plot(cars), paste0(fn, ".pdf"), temp_path,
+f_call <- expression(save_plot(plot(cars), paste0(fn, ".pdf"), temp_path,
                          base = TRUE, save_data = FALSE))
 expect_error(eval(f_call), pattern = "file ending")
 
-f_call <- expr(save_plot(plot(cars), fn, temp_path,
+f_call <- expression(save_plot(plot(cars), fn, temp_path,
                          base = TRUE, save_data = TRUE))
 expect_error(eval(f_call), pattern = "saving")
 
 ## saving data as either xlsx or csv
-dt <- data.table(cars)
-f_call <- expr(save_plot(plot(cars), fn, temp_path,
+dt <- data.frame(cars)
+f_call <- expression(save_plot(plot(cars), fn, temp_path,
                          base = TRUE, save_data = TRUE,
                          data = dt))
 file <- file.path(temp_path, "plot_data", paste0(fn, ".xlsx"))
 eval(f_call)
 expect_true(file.exists(file))
 
-f_call <- expr(save_plot(plot(cars), fn, temp_path,
+f_call <- expression(save_plot(plot(cars), fn, temp_path,
                          base = TRUE, save_data = TRUE,
                          data = dt, excel = FALSE))
 file <- file.path(temp_path, "plot_data", paste0(fn, ".csv"))
