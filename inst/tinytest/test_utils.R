bd0eefe9a7bf97d1b612a9a789afee0e2f70b986
Konstantin <konstantin.hoffie@ba-sachsen.de>
2023-03-20 11:37:42 +0100

    MigStat::: removed

--- inst/tinytest/test_utils.R
+++ inst/tinytest/test_utils.R
@@ -1,13 +1,13 @@
-ex_dat <- MigStat:::read_examples() ## still reads AGS cols as character...
+ex_dat <- read_examples() ## still reads AGS cols as character...
 mig <- ex_dat$mig
 shps <- ex_dat$shps
-shps <- MigStat:::read_clean_shps("/home/konstantin/extdata/shapes31simple2", type = "ags")
+shps <- read_clean_shps("/home/konstantin/extdata/shapes31simple2", type = "ags")
 
 
 #### testing do_join
 n_cols <- ncol(mig)
 mig[, "EF02U2" := as.integer(EF02U2)]
-mig <- MigStat:::do_join(dt1 = mig, dt2 = shps$states,
+mig <- do_join(dt1 = mig, dt2 = shps$states,
                          join_col = "GEN", key1 = "EF02U2", key2 = "AGS",
                          new_col = "st_o")
 n_st <- data.table::uniqueN(mig[, EF02U2])
