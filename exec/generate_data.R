633c1d9b4c0381cce713f9e159384e492bc61cfe
Konstantin <konstantin.hoffie@ba-sachsen.de>
2023-03-21 10:22:26 +0100

    inkar subset created and saved

--- exec/generate_data.R
+++ exec/generate_data.R
@@ -51,4 +51,26 @@ inkar <- read_inkar(file.path(ps$inkar, "inkar_2021.csv"), leading_0 = FALSE)
 ### samples new Migstat rows
 new_rows <- n_new_rows(ex_dat$mig, ex_dat$shps, "mu", "mu", 100)
 
-
+######################################################################
+###################### Subset INKAR data set #########################
+######################################################################
+## This section subsets the INKAR data. The INKAR data can not be used
+## in total at the FDZ. Instead I had to fill in the FDZ
+## Verfahrensbeschreibung and add all INKAR variables that I want to
+## use in the analyses. These variables I saved in a file
+## "fdz_v8.csv". I use this file to subset the INKAR data so that it
+## only contains these variables. This subset I then send to the FDZ.
+inkar_vars <- "~/extdata/inkar_additions/fdz_v8.csv"
+inkar_vars <- fread(inkar_vars)[, .(varname, Kurzname)]
+inkar <- read_inkar(file.path(ps$inkar, "inkar_2021.csv"))
+### subesetting INKAR
+raumbezug <- c("Bundesländer", "Kreise", "Gemeinden")
+inkar <- inkar[Raumbezug %in% raumbezug, ]
+### check if all indicators that are part of the FDZ
+### Verfahrensbeschreibung and thus of "inkar_vars" can be found in
+### the data.
+indikatoren <- inkar[, unique(Indikator)]
+setdiff(inkar_vars[, Kurzname], indikatoren)
+inkar <- inkar[Indikator %in% inkar_vars[, Kurzname]]
+fpath <- "~/extdata/inkar_additions/inkar_fdz.csv"
+fwrite(inkar, fpath)
