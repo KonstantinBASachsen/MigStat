001f729dca31b74327e077dafea14731fea4801e
Konstantin <konstantin.hoffie@ba-sachsen.de>
2023-04-04 11:31:57 +0200

    comment

--- R/shapefiles.R
+++ R/shapefiles.R
@@ -386,11 +386,18 @@ read_shapes <- function(path, year = 2014) {
     #### type = 6 makes sure that geometry is of type
     #### sfc_MULTIPOLYGON. See ?sf::read_sf
     munis <- sf::read_sf(file.path(path, muni_file), type = 6)
-##     munis <- right_crs(munis)
+    ## 04.04.23 right_crs does not work anymore, it fails with Error:
+ ##   OGRCreateCoordinateTransformation(): transformation not
+ ##   available. I dont know if this is still necessary, the crs seems
+ ##   always to be Projected CRS: WGS 84 / UTM zone 32N which is
+ ##   fine. Maybe I don't need the tranformation anymore because I
+ ##   also copy the .prj files. I think before I did not do it.
+    
+    ## munis <- right_crs(munis)
     munis <- data.table::setDT(munis)
 
     states <- sf::read_sf(file.path(path, states_file), type = 6)
-##     states <- right_crs(states)
+##    states <- right_crs(states)
     states <- data.table::setDT(states)
 
     districts <- sf::read_sf(file.path(path, districts_file), type = 6)
