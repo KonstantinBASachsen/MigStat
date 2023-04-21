5b6c7d2fff31fdb0e1a86eec3a8877602fa938bc
Konstantin <konstantin.hoffie@ba-sachsen.de>
2023-04-21 11:40:13 +0200

    tests if origins, destinations, both or neither are missing

--- inst/tinytest/test_correct_region_changes.R
+++ inst/tinytest/test_correct_region_changes.R
@@ -16,13 +16,50 @@ expect_error(correct_flows(flows = flows, dt = shp),
 expect_warning(correct_flows(flows = flows, dt = correct),
                pattern = "AGS were not found")
 
-## in 2012 all ags are found so flows after adustment should be the
-## same
-expect_message(correct_flows(flows = flows[year == 2012], dt = correct),
-               pattern = "as expected")
 ## if there are other grouping variables present, correct_flows won't
 ## produce the correct results
 flows <- get_flows(mig, "di", "di",
                    by = c("year", "EF25"))
 expect_error(correct_flows(flows = flows[year == 2012], dt = correct),
              pattern = "not supposed to be in")
+
+## there might be origins that are not found and there might be
+## destinations that are not found. Here I test for all four
+## combinations of found/ not found if the correct message/ warnings
+## are emitted. In the data for the years 2011 to 2015 all ags are
+## found. For all of the other years ags are missing for both origin
+## and destination.
+
+### I currently only test if ags were found. I do not test for the
+### reported number of flows
+
+## all ags are found
+## in 2012 all ags are found so flows after adustment should be the
+## same
+expect_message(correct_flows(flows = flows[year == 2012], dt = correct),
+               pattern = "All AGS were found for 'origin'.")
+expect_message(correct_flows(flows = flows[year == 2012], dt = correct),
+               pattern = "All AGS were found for 'destination'.")
+
+
+## all origins are found, destinations are missing
+not_found <- setdiff(flows[year == 2000, unique(origin)], correct[year == 2000, ags_old])
+flows_test <- flows[! origin %in% not_found & year == 2000]
+expect_message(correct_flows(flows_test, correct),
+               pattern = "All AGS were found for 'origin'.")
+expect_warning(correct_flows(flows_test, correct),
+               pattern = "Several AGS were not found for 'destination'!")
+
+## all destinations are found, origins are missing
+not_found <- setdiff(flows[year == 2000, unique(destination)], correct[year == 2000, ags_old])
+flows_test <- flows[! destination %in% not_found & year == 2000]
+expect_message(correct_flows(flows_test, correct),
+               pattern = "All AGS were found for 'destination'.")
+expect_warning(correct_flows(flows_test, correct),
+               pattern = "Several AGS were not found for 'origin'!")
+
+## both: origins and destinations are missing
+expect_warning(correct_flows(flows[year == 2000, ], correct[year == 2000]),
+               pattern = "Several AGS were not found for 'origin'!")
+expect_warning(correct_flows(flows[year == 2000, ], correct[year == 2000]),
+               pattern = "Several AGS were not found for 'destination'!")
