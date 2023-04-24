538e96080d8def5e1f4ae5de48a21336da94dbff
Konstantin <konstantin.hoffie@ba-sachsen.de>
2023-04-24 11:25:50 +0200

    tests for specific corrections

--- inst/tinytest/test_correct_region_changes.R
+++ inst/tinytest/test_correct_region_changes.R
@@ -57,3 +57,36 @@ expect_warning(correct_flows(flows[year == 2000, ], correct[year == 2000]),
                pattern = "Several AGS were not found for 'origin'!")
 expect_warning(correct_flows(flows[year == 2000, ], correct[year == 2000]),
                pattern = "Several AGS were not found for 'destination'!")
+
+######################################################################
+########### Test if specific corrections are correct #################
+######################################################################
+### No changes, same ags, so nothing should be changed
+cor_test <- correct[year == 1990]
+test_flows <- data.table(origin = c(1001, 15003),
+                         destination = c(1051, 15003),
+                         flow = c(100, 1000),
+                         year = 1990)
+test_flows2 <- correct_flows(test_flows, correct)
+expect_equal(test_flows2[, flow], test_flows[, flow])
+### Eingemeindungen. Parts if the origins in the next test_flows were
+### all matched to Sömmerda 16068. This means that parts of the flows
+### of the origins have to be matched to 16068.
+test_flows <- data.table(origin = c(16014, 16018, 16038),
+                         destination = c(1051, 15003, 1004),
+                         flow = c(100, 1000, 500),
+                         year = 1990)
+test_flows2 <- correct_flows(test_flows, correct)
+### by looking at cor_test[ags_new == 16068] I determine the number of
+### flows from 16068
+expected <- round(100 * 0.03788186 + 1000 * 0.37883162 + 500 * 1)
+expect_equal(test_flows2[origin == 16068, sum(flow)], expected)
+
+### same as above but with the incorporated municipalities as
+### destinations
+test_flows <- data.table(origin = c(1051, 15003, 1004),
+                         destination = c(16014, 16018, 16038),
+                         flow = c(100, 1000, 500),
+                         year = 1990)
+test_flows2 <- correct_flows(test_flows, correct)
+expect_equal(test_flows2[destination == 16068, sum(flow)], expected)
