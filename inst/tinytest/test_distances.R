681a30ea624aaaf4cddbc0c5409b4ba81cd3ba46
Konstantin <konstantin.hoffie@ba-sachsen.de>
2023-05-02 13:37:52 +0200

    tests outcommented... read_distances() not really used anymore

--- inst/tinytest/test_distances.R
+++ inst/tinytest/test_distances.R
@@ -1,7 +1,8 @@
 paths <- make_paths()
 dist_f <- file.path(paths$dist, "distances_st.csv")
-## expect_warning(read_distances(dist_f), "integer") ## I think bc
-##                                                   ## distance_pos and
-##                                                   ## distance_centroid
-##                                                   ## are included
+## Currently I do not use read_distances because I just compute them
+## using get_distances. Will change when I analyze municipalities
+
+## expect_warning(read_distances(dist_f), "integer") ## I think bc ##
+## distance_pos and ## distance_centroid ## are included
 ## expect_warning(read_distances(dist_f, type = "cha"), "integer")
